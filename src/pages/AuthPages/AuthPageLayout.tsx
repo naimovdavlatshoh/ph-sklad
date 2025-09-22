0e683316fba4d4d6b1585f7d20ff61e89663ea79
naimovdavlatshoh <you@example.com>
2025-09-22 12:12:39 +0500

    changed site name

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -28,7 +28,7 @@ export default function AuthLayout({
               <p className="text-center text-gray-400 dark:text-white/60">
                 Free and Open-Source Tailwind CSS Admin Dashboard Template
               </p> */}
-              <p className="text-6xl text-white">PH-Sklad</p>
+              <p className="text-6xl text-white">СКЛАД</p>
             </div>
           </div>
         </div>
